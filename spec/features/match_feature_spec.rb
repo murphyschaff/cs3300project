f5135e97560435516939ba0987ce630743f853da
Murphy Schaff <mschaff@uccs.edu>
2023-04-21 20:14:19 +0000

    circle ci part integration and finished factorybot testing

--- spec/features/match_feature_spec.rb
+++ spec/features/match_feature_spec.rb
@@ -1,6 +1,28 @@
 require "rails_helper"
 
 RSpec.feature "Matches", type: :feature do
+  context "Login" do
+    scenario "should sign up" do
+      visit root_path
+      click_link 'Sign up'
+      within("form") do
+        fill_in "Email", with: "testing@test.com"
+        fill_in "Password", with: "123456"
+        fill_in "Password confirmation", with: "123456"
+        click_button "Sign up"
+      end
+      expect(page).to have_content("Welcome! You have signed up successfully.")
+    end
+
+
+    scenario "should log in" do
+      user = FactoryBot.create(:user)
+      login_as(user)
+      visit root_path
+      expect(page).to have_content("Logged in")
+    end
+  end
+
     context "Update match" do
       let(:match) { Match.create(opponent: "dude", score: 12, location: "denver", notes: "hi") }
       before(:each) do
